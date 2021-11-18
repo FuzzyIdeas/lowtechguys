f9493efbe1c2ade5a7199aace6ac1324968110b0
Ovidiu Rusu <ovidiumax1994@gmail.com>
2021-11-18 12:56:07 +0200

    Fix width

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,24 @@ module.exports = {
   purge: ['./public/*.html', './public/**/*.html'],
   mode: 'jit',
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+      '3xl': '1920px',
+      '4xl': '2560px',
+    },
     minHeight: {
       0: '0',
       '1/4': '25%',
